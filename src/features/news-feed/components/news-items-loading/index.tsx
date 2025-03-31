0430318bb9da8194c5691e81f598a9fe910cce66
Sergey <iamgreyy@gmail.com>
2025-03-31 22:09:06 +0500

    feat(news-feed): add mock items on loading

--- src/features/news-feed/components/news-items-loading/index.tsx
+++ src/features/news-feed/components/news-items-loading/index.tsx
@@ -1,13 +1,35 @@
+import { useMemo } from 'react';
+
 import { NewsItem } from '@/features/news-feed/components/news-item';
 import { useAppSelector } from '@/lib/hooks';
+import { NewsFeedItem } from '@/types/news-feed.ts';
 
 export const NewsItemsLoading = () => {
   const loading = useAppSelector((state) => state.newsFeed.loading);
   const newsItems = useAppSelector((state) => state.newsFeed.news);
 
+  const renderingNews = useMemo(
+    () =>
+      loading && !newsItems.length
+        ? Array.from(
+            { length: 6 },
+            (_, index) =>
+              ({
+                id: index,
+                isHot: false,
+                author: '',
+                content: '',
+                title: '',
+                createdTimestamp: new Date().toISOString(),
+              }) as NewsFeedItem
+          )
+        : newsItems,
+    [loading, newsItems]
+  );
+
   return (
     <>
-      {newsItems.map((newsItem) => (
+      {renderingNews.map((newsItem) => (
         <NewsItem key={newsItem.id} {...newsItem} loading={loading} />
       ))}
     </>
